f4ac1df130a255d8dce7238b6896857b5cc8c3b1
SashaSNky <sanek-avto@yandex.by>
2023-02-24 21:03:57 +0300

    init: init and do task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.solution=undefined;
+        this.min=0;
+        this.max=0;
+    }
+    
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+       this.solution=Math.round((this.min + this.max) / 2);
+        return this.solution;
     }
 
     lower() {
-
+        this.max=this.solution;
+        this.solution=Math.round((this.min + this.max) / 2);
+        return this.solution;
     }
 
     greater() {
-
+        this.min=this.solution;
+        this.solution=Math.round((this.min + this.max) / 2);
+        return this.solution;
     }
 }
 
